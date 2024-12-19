14836b60c2fd707c201d80d25e9849841898aa1c
PekahZero <729421179@qq.com>
2024-12-19 17:54:42 +0800

    multi dataset

--- run.py
+++ run.py
@@ -19,8 +19,8 @@ def init_spider_message(idx: int, item: dict) -> dict:
     :param item: one sample of dataset
     :return: initial message object of group chat
     """
-    db_id, query, evidence, gt = item['db_id'], item['question'], str(""), item['query']
-    difficulty = eval_hardness(item['sql'])
+    db_id, query, evidence, gt = item["db_id"], item["question"], str(""), item["query"]
+    difficulty = eval_hardness(item["sql"])
     user_message = {
         "idx": idx,
         "db_id": db_id,
@@ -29,25 +29,29 @@ def init_spider_message(idx: int, item: dict) -> dict:
         "extracted_schema": {},
         "ground_truth": gt,
         "difficulty": difficulty,
-        "send_to": SYSTEM_NAME
+        "send_to": SYSTEM_NAME,
     }
     return user_message
 
 
-def init_bird_message(idx: int, item: dict, db_path: str=None, use_gold_schema: bool = False) -> dict:
+def init_bird_message(
+    idx: int, item: dict, db_path: str = None, use_gold_schema: bool = False
+) -> dict:
     """
     Construct message for text-to-SQL task
     :param idx: start from 0
     :param item: one sample of dataset
     :return: initial message object of group chat
     """
-    db_id, query, evidence, gt, difficulty = item['db_id'], \
-                                             item['question'], \
-                                             item['evidence'], \
-                                             item.get('SQL', ''), \
-                                             item.get('difficulty', 'simple')
-    
-    gold_schema_path = './data/bird/dev_gold_schema.json'
+    db_id, query, evidence, gt, difficulty = (
+        item["db_id"],
+        item["question"],
+        item["evidence"],
+        item.get("SQL", ""),
+        item.get("difficulty", "simple"),
+    )
+
+    gold_schema_path = "./data/bird/dev_gold_schema.json"
     gold_schema = {}
     all_gold_schema_dict = {}
     key = f"{db_id.strip()}\t{query.strip()}"
@@ -58,7 +62,7 @@ def init_bird_message(idx: int, item: dict, db_path: str=None, use_gold_schema: 
             gold_schema = all_gold_schema_dict[key]
         else:
             raise ValueError(f"Can't find gold schema for {key}")
-    
+
     user_message = {
         "idx": idx,
         "db_id": db_id,
@@ -67,19 +71,81 @@ def init_bird_message(idx: int, item: dict, db_path: str=None, use_gold_schema: 
         "extracted_schema": gold_schema if gold_schema else {},
         "ground_truth": gt,
         "difficulty": difficulty,
-        "send_to": SYSTEM_NAME
+        "send_to": SYSTEM_NAME,
     }
     return user_message
 
 
-def run_batch(dataset_name, input_file, output_file, db_path, tables_json_path, start_pos=0, log_file=None, dataset_mode='dev', use_gold_schema=False, without_selector=False):
-    chat_manager = ChatManager(data_path=db_path,
-                               tables_json_path=tables_json_path,
-                               log_path=log_file,
-                               dataset_name=dataset_name,
-                               model_name='gpt-4',
-                               lazy=True,
-                               without_selector=without_selector)
+def init_science_message(
+    idx: int, item: dict, db_path: str = None, use_gold_schema: bool = False
+) -> dict:
+    """
+    Construct message for text-to-SQL task
+    :param idx: start from 0
+    :param item: one sample of dataset
+    :return: initial message object of group chat
+    """
+    use_gold_schema = False
+    db_id, query, evidence, gt, difficulty = (
+        item["db_id"],
+        item["question"],
+        item["evidence"],
+        item.get("SQL", ""),
+        item.get(
+            "difficulty", item["db_id"]
+        ),  # science 没有苦难程度信息, 使用数据库的信息代替
+    )
+
+    gold_schema_path = "./data/science/dev_gold_schema.json"
+    gold_schema = {}
+    all_gold_schema_dict = {}
+    key = f"{db_id.strip()}\t{query.strip()}"
+    if use_gold_schema:
+        if os.path.exists(gold_schema_path):
+            all_gold_schema_dict = load_json_file(gold_schema_path)
+        if key in all_gold_schema_dict:
+            gold_schema = all_gold_schema_dict[key]
+        else:
+            raise ValueError(f"Can't find gold schema for {key}")
+
+    user_message = {
+        "idx": idx,
+        "db_id": db_id,
+        "query": query,
+        "evidence": evidence,
+        "extracted_schema": gold_schema if gold_schema else {},
+        "ground_truth": gt,
+        "difficulty": difficulty,
+        "send_to": SYSTEM_NAME,
+    }
+    return user_message
+
+
+def run_batch(
+    dataset_name,
+    input_file,
+    output_file,
+    db_path,
+    tables_json_path,
+    start_pos=0,
+    log_file=None,
+    dataset_mode="dev",
+    use_gold_schema=False,
+    without_selector=False,
+):
+    chat_manager = ChatManager(
+        data_path=db_path,
+        tables_json_path=tables_json_path,
+        log_path=log_file,
+        dataset_name=dataset_name,
+        model_name="gpt-4o-mini",
+        #    model_name='gpt-4',
+        lazy=True,
+        without_selector=without_selector,
+    )
+
+    # print("Start to run batch...")
+    chat_manager.ping_network()
     # load dataset
     batch = load_json_file(input_file)
     # resume from last checkpoint
@@ -87,95 +153,142 @@ def run_batch(dataset_name, input_file, output_file, db_path, tables_json_path, 
     if os.path.exists(output_file):
         output_data_lst = load_jsonl_file(output_file)
         for o in output_data_lst:
-            finished_ids.add(o['idx'])
-    unfinished_ids = [n for n in range(len(batch)) if n not in finished_ids and n >= start_pos]
+            finished_ids.add(o["idx"])
+    unfinished_ids = [
+        n for n in range(len(batch)) if n not in finished_ids and n >= start_pos
+    ]
     print(f"len(unfinished_data) = {len(unfinished_ids)}")
 
     # add question_id if needed
     for k, item in enumerate(batch):
-        if 'question_id' not in item:
-            item['question_id'] = k
+        if "question_id" not in item:
+            item["question_id"] = k
 
+    # 主要是针对bird 没有train set的问题
+    # 有些db_id需要排除
     # skip some json data
     excluded_db_ids = []
-    if dataset_mode == 'train':
-        exclude_txt = './data/bird_train/excluded_db_ids.txt'
+    if dataset_mode == "train":
+        exclude_txt = "./data/bird_train/excluded_db_ids.txt"
         excluded_db_ids = read_txt_file(exclude_txt)
     new_batch = []
-    exclude_db_json_cnt = 0 # for exclude some dbs in bird train set
+    exclude_db_json_cnt = 0  # for exclude some dbs in bird train set
     for k, item in enumerate(batch):
-        q_id = item['question_id']
+        q_id = item["question_id"]
         if q_id not in unfinished_ids:
             continue
-        if dataset_mode == 'train':
+        if dataset_mode == "train":
             # skip excluded db_id
-            if item['db_id'] in excluded_db_ids:
+            if item["db_id"] in excluded_db_ids:
                 exclude_db_json_cnt += 1
                 continue
         new_batch.append(item)
-    
+
     if exclude_db_json_cnt:
         print(f"excluded {exclude_db_json_cnt} excluded db json data")
     time.sleep(2)
     batch = new_batch
 
-
+    # ------------ 开始生成SQL ------------
     # generate SQL one by one, and save result one by one
-    with open(output_file, 'a+', encoding='utf-8') as fp:
+    with open(output_file, "a+", encoding="utf-8") as fp:
         total_num = len(batch)
         for cur_idx, item in tqdm(enumerate(batch), total=total_num):
-            idx = item['question_id']
-            db_id = item['db_id']
+            idx = item["question_id"]
+            db_id = item["db_id"]
             print(f"\n\nprocessing: {cur_idx}/{total_num}\n\n", flush=True)
-            if idx not in unfinished_ids: continue
+            if idx not in unfinished_ids:
+                continue
             if dataset_name == "spider":
-                user_message = init_spider_message(idx, item)  # imitate user send a question to system
+                user_message = init_spider_message(
+                    idx, item
+                )  # imitate user send a question to system
             elif dataset_name == "bird":
-                user_message = init_bird_message(idx, item, db_path=db_path, use_gold_schema=use_gold_schema)  # imitate user send a question to system
+                user_message = init_bird_message(
+                    idx, item, db_path=db_path, use_gold_schema=use_gold_schema
+                )  # imitate user send a question to system
+
+            # SCIENCE
+            else:
+                user_message = init_science_message(
+                    idx, item, db_path=db_path, use_gold_schema=use_gold_schema
+                )  # imitate user send a question to system
             try:
                 chat_manager.start(user_message)
                 try:
-                    del user_message['desc_str']
-                    del user_message['fk_str']
-                    del user_message['send_to']
+                    del user_message["desc_str"]
+                    del user_message["fk_str"]
+                    del user_message["send_to"]
                 except:
                     pass
                 print(json.dumps(user_message, ensure_ascii=False), file=fp, flush=True)
             except Exception as e:
                 # for debug
                 traceback.print_exc()
-                print(f"Exception: {e}, sleep 20 seconds.", flush=True)
-                time.sleep(20)
+                print(f"Exception: {e}, sleep 10 seconds.", flush=True)
+                time.sleep(10)
                 # raise Exception(str(e))
             print(f"\n\ndeal {cur_idx+1}/{total_num} done!\n\n")
         print(f"Result dump into {output_file}", file=sys.stdout, flush=True)
 
     # export evaluation results
     out_dir = os.path.dirname(output_file)
-    
+
     # transfer SQL result to supportable BIRD format
+    # BIRD
     if dataset_name == "bird":
         evaluation_file_path = f"{out_dir}/predict_{dataset_mode}.json"
-        with open(evaluation_file_path, 'w', encoding='utf8') as fout:
+        with open(evaluation_file_path, "w", encoding="utf8") as fout:
             output_json_list = load_jsonl_file(output_file)
-            output_json_list = sorted(output_json_list, key=lambda i: i['idx'])
+            output_json_list = sorted(output_json_list, key=lambda i: i["idx"])
             eval_tuple_lst = []
             for o in output_json_list:
-                pred_sql = o['pred'].strip()
+                pred_sql = o["pred"].strip()
                 pred_sql = replace_multiple_spaces(pred_sql)
-                sql_and_db_str = pred_sql + '\t' + '----- bird -----' + '\t' + o['db_id']
-                obj = [o['query'], sql_and_db_str]
+                sql_and_db_str = (
+                    pred_sql + "\t" + "----- bird -----" + "\t" + o["db_id"]
+                )
+                obj = [o["query"], sql_and_db_str]
                 eval_tuple_lst.append(obj)
             json.dump(eval_tuple_lst, fp=fout, ensure_ascii=False, indent=2)
             print(f"BIRD format file dump into {evaluation_file_path}")
+    # SCIENCE
+    elif dataset_name == "science":
+
+        evaluation_file_path_SQL = f"{out_dir}/pred_{dataset_mode}.sql"
+        spider_sql_lst = []
+        output_json_lst = load_jsonl_file(output_file)
+        for output_json in output_json_lst:
+            pred_sql = output_json["pred"]
+            pred_sql = replace_multiple_spaces(pred_sql)
+            spider_sql_lst.append(pred_sql.strip() + "\n")
+        save_file(evaluation_file_path_SQL, spider_sql_lst)
+
+        evaluation_file_path = f"{out_dir}/predict_{dataset_mode}.json"
+        with open(evaluation_file_path, "w", encoding="utf8") as fout:
+            output_json_list = load_jsonl_file(output_file)
+            output_json_list = sorted(output_json_list, key=lambda i: i["idx"])
+            eval_tuple_lst = []
+            for o in output_json_list:
+                pred_sql = o["pred"].strip()
+                pred_sql = replace_multiple_spaces(pred_sql)
+                sql_and_db_str = (
+                    pred_sql + "\t" + "-----science-----" + "\t" + o["db_id"]
+                )
+                obj = [o["query"], sql_and_db_str]
+                eval_tuple_lst.append(obj)
+            json.dump(eval_tuple_lst, fp=fout, ensure_ascii=False, indent=2)
+
+        print(f"SCIENCE format file dump into {evaluation_file_path}")
+    # SPIDER
     elif dataset_name == "spider":
         evaluation_file_path = f"{out_dir}/pred_{dataset_mode}.sql"
         spider_sql_lst = []
         output_json_lst = load_jsonl_file(output_file)
         for output_json in output_json_lst:
-            pred_sql = output_json['pred']
+            pred_sql = output_json["pred"]
             pred_sql = replace_multiple_spaces(pred_sql)
-            spider_sql_lst.append(pred_sql.strip() + '\n')
+            spider_sql_lst.append(pred_sql.strip() + "\n")
         save_file(evaluation_file_path, spider_sql_lst)
         print(f"Spider format file dump into {evaluation_file_path}")
     else:
@@ -193,16 +306,40 @@ def check_all_paths(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset_name', type=str, default='spider', choices=['spider', 'bird'], help='dataset name')
-    parser.add_argument('--dataset_mode', type=str, default='dev', choices=['train', 'dev', 'test'], help='dataset mode')
-    parser.add_argument('--input_file', type=str, required=True, help='path to dataset input')
-    parser.add_argument('--db_path', type=str, required=True, help='path to databases in dataset')
-    parser.add_argument('--tables_json_path', type=str, default=None, help='path to tables.json')
-    parser.add_argument('--output_file', type=str, required=True, help='path to predicted output')
-    parser.add_argument('--log_file', type=str, default='', help='path to log file if needed')
-    parser.add_argument('--start_pos', type=int, default=0, help='start position of a batch')
-    parser.add_argument('--use_gold_schema', action='store_true', default=False)
-    parser.add_argument('--without_selector', action='store_true', default=False)
+    parser.add_argument(
+        "--dataset_name",
+        type=str,
+        default="spider",
+        choices=["spider", "bird", "science"],
+        help="dataset name",
+    )
+    parser.add_argument(
+        "--dataset_mode",
+        type=str,
+        default="dev",
+        choices=["train", "dev", "test"],
+        help="dataset mode",
+    )
+    parser.add_argument(
+        "--input_file", type=str, required=True, help="path to dataset input"
+    )
+    parser.add_argument(
+        "--db_path", type=str, required=True, help="path to databases in dataset"
+    )
+    parser.add_argument(
+        "--tables_json_path", type=str, default=None, help="path to tables.json"
+    )
+    parser.add_argument(
+        "--output_file", type=str, required=True, help="path to predicted output"
+    )
+    parser.add_argument(
+        "--log_file", type=str, default="", help="path to log file if needed"
+    )
+    parser.add_argument(
+        "--start_pos", type=int, default=0, help="start position of a batch"
+    )
+    parser.add_argument("--use_gold_schema", action="store_true", default=False)
+    parser.add_argument("--without_selector", action="store_true", default=False)
     args = parser.parse_args()
     # 打印args中的键值对
     for key, value in vars(args).items():
@@ -225,5 +362,5 @@ if __name__ == "__main__":
         log_file=args.log_file,
         start_pos=args.start_pos,
         use_gold_schema=args.use_gold_schema,
-        without_selector=args.without_selector
+        without_selector=args.without_selector,
     )
--- run.sh
+++ run.sh
@@ -7,19 +7,25 @@
 
 # Generate SQL on foo dataset for env test
 # This will get ./outputs/foo/output_bird.json and ./outputs/foo/predict_test.json
-python ./run.py --dataset_name "bird" \
-   --dataset_mode="test" \
-   --input_file "./data/foo/test.json" \
-   --db_path "./data/foo/test_databases" \
-   --tables_json_path "./data/foo/test_tables.json" \
-   --output_file "./outputs/foo/output_bird.json" \
-   --log_file "./outputs/foo/log.txt"
-
-echo "Generate SQL on env test data done!"
+# proxy 
+source activate macsql
+export http_proxy="socks5h://10.176.24.76:10802"
+export https_proxy="socks5h://10.176.24.76:10802"
 
 
-# #################### BIRD dev 【run】count=1534 #########
-# Generate SQL on BIRD dev dataset
+# python ./run.py --dataset_name "bird" \
+#    --dataset_mode="test" \
+#    --input_file "./data/foo/test.json" \
+#    --db_path "./data/foo/test_databases" \
+#    --tables_json_path "./data/foo/test_tables.json" \
+#    --output_file "./outputs/foo/output_bird.json" \
+#    --log_file "./outputs/foo/log.txt"
+
+# echo "Generate SQL on env test data done!"
+
+
+#################### BIRD dev 【run】count=1534 #########
+# # Generate SQL on BIRD dev dataset
 # python ./run.py --dataset_name="bird" \
 #    --dataset_mode="dev" \
 #    --input_file="./data/bird/dev.json" \
@@ -44,14 +50,15 @@ echo "Generate SQL on env test data done!"
 
 
 # #################### Spider dev 【run】count=1034 #########
-# Generate SQL on BIRD dev dataset
+# Generate SQL on SPIDER dev dataset
 # python ./run.py --dataset_name "spider" \
 #    --dataset_mode="dev" \
 #    --input_file "./data/spider/dev.json" \
 #    --db_path "./data/spider/database" \
 #    --tables_json_path "./data/spider/tables.json" \
 #    --output_file "./outputs/spider/output_spider.json" \
-#    --log_file "./outputs/spider/log.txt"
+#    --log_file "./outputs/spider/log.txt" \
+#    --start_pos 0 \
 
 # #################### Spider dev 【evaluation】EX and EM count=1034 #########
 # python ./evaluation/evaluation_spider.py \
@@ -60,5 +67,17 @@ echo "Generate SQL on env test data done!"
 #    --table "./data/spider/tables.json" \
 #    --pred "./outputs/spider/pred_dev.sql" \
 #    --etype "exec"
+
+# echo "Done!"
+
+#################### SCIENCE dev 【run】count=1534 #########
+# # Generate SQL on SCIENCE dev dataset
+python ./run.py --dataset_name="science" \
+   --dataset_mode="dev" \
+   --input_file="./data/science/dev/dev.json" \
+   --db_path="./data/science/dev/dev_databases/" \
+   --tables_json_path "./data/science/dev/dev_tables.json" \
+   --output_file="./outputs/science/output_science.json" \
+   --log_file="./outputs/science/log.txt"
 
 echo "Done!"
